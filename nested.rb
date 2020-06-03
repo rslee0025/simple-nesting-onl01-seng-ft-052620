52dc69d3de7dbe9857bcd656d0841bbc8451ba2a
Rachel S. Lee <rslee0025@gmail.com>
2020-06-03 18:53:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -89,7 +89,9 @@ def adding_matz
         }
      }
 
-  programmer_hash {:yukihiro_matsumoto}[:known_for]
+  programmer_hash {:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  
+  
     
   
   
