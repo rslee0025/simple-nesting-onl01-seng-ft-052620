8327c860f1a37257511bab78999fa6432ec56284
Rachel S. Lee <rslee0025@gmail.com>
2020-06-03 18:12:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,10 +15,11 @@ def hopper
           :languages => ["C"]
         }
      }
+return programmer_hash [:grace_hopper]
 
 end
 
-puts programmer_hash [:grace_hopper]
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
